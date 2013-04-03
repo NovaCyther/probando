3c290a7645295a1bbcf667da9e67f44500ccac26
Luis <luistotosc@gmail.com>
2013-04-03 09:22:09 -0600

    testing

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+//1
 int main(int argc, char **argv)
 {
 	char name[255];
@@ -9,7 +9,8 @@ int main(int argc, char **argv)
 	fgets(name, 255, stdin);
 	printf("length = %d\n", strlen(name)); /* debug line */
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
-
+//2
 	printf("Hello %s!\n", name);
 	return 0;
+	//3
 }
